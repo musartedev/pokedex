02468fd869c8f50e258b8ed306ca2bbcbadde988
Mariangelica Useche <mariangelica.useche@gmail.com>
2019-06-23 14:37:52 -0400

    Init login page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from "react";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
+import Login from "./pages/Login";
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Button, Checkbox, Form, Image } from "semantic-ui-react";
+
+import "./Styles/Login.css";
+import pokemonLogo from "../images/pokemon.svg";
+
+const FormExampleForm = () => (
+  <Form className="FormExampleForm">
+    <Form.Field>
+      <label>Email</label>
+      <input placeholder="Email" type="email" />
+    </Form.Field>
+    <Form.Field>
+      <label>Password</label>
+      <input placeholder="Password" type="password" />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox label="Remenber me" />
+    </Form.Field>
+    <Button type="submit">Send</Button>
+  </Form>
+);
+
+export default class Login extends React.Component {
+  render() {
+    return (
+      <div className="Login">
+        <div
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%, -50%)"
+          }}
+          className="Login__form-container"
+        >
+          <Image className="Login__logo" size="small" src={pokemonLogo} />
+          <p> Enter for more</p>
+          {/* <h2>Login</h2> */}
+          <FormExampleForm />
+        </div>
+      </div>
+    );
+  }
+}
